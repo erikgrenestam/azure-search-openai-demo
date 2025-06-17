4ce36fc38aad3729b56d526d88b49052b5eea21a
Erik Grenestam <egr@nationalbanken.dk>
2025-06-18 00:15:48 +0200

    keep en and da languages.

--- app/frontend/src/i18n/config.ts
+++ app/frontend/src/i18n/config.ts
@@ -5,14 +5,7 @@ import { initReactI18next } from "react-i18next";
 
 import daTranslation from "../locales/da/translation.json";
 import enTranslation from "../locales/en/translation.json";
-import esTranslation from "../locales/es/translation.json";
-import frTranslation from "../locales/fr/translation.json";
-import jaTranslation from "../locales/ja/translation.json";
-import nlTranslation from "../locales/nl/translation.json";
-import ptBRTranslation from "../locales/ptBR/translation.json";
-import trTranslation from "../locales/tr/translation.json";
-import itTranslation from "../locales/it/translation.json";
-import plTranslation from "../locales/pl/translation.json";
+
 
 export const supportedLngs: { [key: string]: { name: string; locale: string } } = {
     da: {
@@ -22,38 +15,6 @@ export const supportedLngs: { [key: string]: { name: string; locale: string } } 
     en: {
         name: "English",
         locale: "en-US"
-    },
-    es: {
-        name: "Español",
-        locale: "es-ES"
-    },
-    fr: {
-        name: "Français",
-        locale: "fr-FR"
-    },
-    ja: {
-        name: "日本語",
-        locale: "ja-JP"
-    },
-    nl: {
-        name: "Nederlands",
-        locale: "nl-NL"
-    },
-    ptBR: {
-        name: "Português Brasileiro",
-        locale: "pt-BR"
-    },
-    tr: {
-        name: "Türkçe",
-        locale: "tr-TR"
-    },
-    it: {
-        name: "Italiano",
-        locale: "it-IT"
-    },
-    pl: {
-        name: "Polski",
-        locale: "pl-PL"
     }
 };
 
@@ -66,15 +27,7 @@ i18next
     .init({
         resources: {
             da: { translation: daTranslation },
-            en: { translation: enTranslation },
-            es: { translation: esTranslation },
-            fr: { translation: frTranslation },
-            ja: { translation: jaTranslation },
-            nl: { translation: nlTranslation },
-            ptBR: { translation: ptBRTranslation },
-            tr: { translation: trTranslation },
-            it: { translation: itTranslation },
-            pl: { translation: plTranslation }
+            en: { translation: enTranslation }
         },
         fallbackLng: "en",
         supportedLngs: Object.keys(supportedLngs),
